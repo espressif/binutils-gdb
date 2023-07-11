dca6dc5a555e805c9333f62a29c573c3c2fea6c0
Wilko Nienhaus <wilko.nienhaus@gmail.com>
2023-07-11 22:32:26 +0300

    esp32ulp: esp32s2: fix sub_opcode for STO instruction
    As per ESP32-S2/S3 Technical Reference Manual, the sub_opcode for
    Automatic Storage Mode ST-OFFSET should be 3, not 2.
    
    See page 35, figure 1-11, of the ESP32-S2 TRM v1.1 (2022-09-23)
    as well as page 305, figure 2-11, of the ESP32-S3 TRM v1.3 (2023-07-04)
    
    Closes https://github.com/espressif/binutils-gdb/pull/2

--- gas/config/tc-esp32ulp_esp32s2.c
+++ gas/config/tc-esp32ulp_esp32s2.c
@@ -449,7 +449,7 @@ INSTR_T
 esp32ulp_wr_mem_sto_esp32s2 (Expr_Node * offset)
 {
   int addr_val = EXPR_VALUE (offset);
-  unsigned int local_op = WR_MEM (0, 0, addr_val >> 2, 0, 0, 0, 0x2);
+  unsigned int local_op = WR_MEM (0, 0, addr_val >> 2, 0, 0, 0, 0x3);
 
   return conscode (gencode (local_op),
 		   Expr_Node_Gen_Reloc (offset, BFD_RELOC_ESP32S2ULP_WR_MEM));
