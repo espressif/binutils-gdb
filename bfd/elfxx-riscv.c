7e6bf8602644933aed4eedbafa5ca47286c339ff
Alexey Lapshin <alexey.lapshin@espressif.com>
2023-11-20 14:22:07 +0400

    RISC-V: add support XEspPIE extension

--- bfd/elfxx-riscv.c
+++ bfd/elfxx-riscv.c
@@ -1471,6 +1471,7 @@ static struct riscv_supported_ext riscv_supported_vendor_x_ext[] =
   {"xcvelw",		ISA_SPEC_CLASS_DRAFT,	1, 0, 0 },
   {"xcvbi",		ISA_SPEC_CLASS_DRAFT,	1, 0, 0 },
   {"xcvmem",		ISA_SPEC_CLASS_DRAFT,	1, 0, 0 },
+  {"xesppie",		ISA_SPEC_CLASS_DRAFT,	1, 0, 0 },
   {"xtheadba",		ISA_SPEC_CLASS_DRAFT,	1, 0, 0 },
   {"xtheadbb",		ISA_SPEC_CLASS_DRAFT,	1, 0, 0 },
   {"xtheadbs",		ISA_SPEC_CLASS_DRAFT,	1, 0, 0 },
@@ -2731,6 +2732,8 @@ riscv_multi_subset_supports (riscv_parse_subset_t *rps,
       return riscv_subset_supports (rps, "xcvbi");
     case INSN_CLASS_XCVMEM:
       return riscv_subset_supports (rps, "xcvmem");
+    case INSN_CLASS_XESPPIE:
+      return riscv_subset_supports (rps, "xesppie");
     case INSN_CLASS_XTHEADBA:
       return riscv_subset_supports (rps, "xtheadba");
     case INSN_CLASS_XTHEADBB:
@@ -3009,6 +3012,8 @@ riscv_multi_subset_supports_ext (riscv_parse_subset_t *rps,
       return "xcvbi";
     case INSN_CLASS_XCVMEM:
       return "xcvmem";
+    case INSN_CLASS_XESPPIE:
+      return "xesppie";
     case INSN_CLASS_XTHEADBA:
       return "xtheadba";
     case INSN_CLASS_XTHEADBB:
