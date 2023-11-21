b40b77320b5681f4d4a045bd2aa82c05ae64cb8e
Alexey Lapshin <alexey.lapshin@espressif.com>
2023-11-21 20:47:56 +0400

    gdb/python: initialize embedding without signals

--- gdb/python/python.c
+++ gdb/python/python.c
@@ -2098,7 +2098,7 @@ do_start_initialization ()
      remain alive for the duration of the program's execution, so
      it is not freed after this call.  */
   Py_SetProgramName (progname_copy);
-  Py_Initialize ();
+  Py_InitializeEx (0);
 #else
   PyConfig config;
 
@@ -2110,6 +2110,7 @@ do_start_initialization ()
 
   config.write_bytecode = python_write_bytecode ();
   config.use_environment = !python_ignore_environment;
+  config.install_signal_handlers = 0;
 
   status = PyConfig_Read (&config);
   if (PyStatus_Exception (status))
@@ -2123,7 +2124,7 @@ init_done:
     return false;
 #endif
 #else
-  Py_Initialize ();
+  Py_InitializeEx (0);
 #endif
 
 #if PY_VERSION_HEX < 0x03090000
