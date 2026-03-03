a17435e57a730489490e21f879bc6f3cb64a5711
Alexey Lapshin <alexey.lapshin@espressif.com>
2026-03-03 14:03:13 +0700

    xtensa: fix windows 8dot3 mixing with XTENSA_GNU_CONFIG

--- bfd/xtensa-dynconfig.c
+++ bfd/xtensa-dynconfig.c
@@ -83,6 +83,63 @@ void xtensa_set_dynconfig_from_argv(int argc, char **argv)
     }
 }
 
+#ifdef HAVE_WINDOWS_H
+
+static int
+is_8dot3_filename (const char *shortPath)
+{
+  TCHAR *longPath = NULL;
+  DWORD bufferSize = MAX_PATH;
+  DWORD result;
+  int ret;
+
+  // Allocate initial buffer
+  longPath = (char *) malloc (bufferSize);
+  if (!longPath)
+    {
+      _bfd_error_handler (_("Memory allocation failed."));
+      abort ();
+    }
+
+  // Call GetLongPathNameA in a loop to handle buffer resizing
+  while (1)
+    {
+      result = GetLongPathNameA (shortPath, longPath, bufferSize);
+      if (result == 0)
+	{
+	  // Error occurred
+	  _bfd_error_handler (_
+			      ("Failed to retrieve the long path. Error code: %lu (%s)"),
+			      GetLastError (), longPath);
+        free (longPath);
+	  abort ();
+	}
+      else if (result > bufferSize)
+	{
+	  // Buffer too small, reallocate with the new size
+	  bufferSize = result;
+	  char *newBuffer = (char *) realloc (longPath, bufferSize);
+	  if (!newBuffer)
+	    {
+	      free (longPath);
+	      _bfd_error_handler (_("Memory allocation failed."));
+	      abort ();
+	    }
+	  longPath = newBuffer;
+	}
+      else
+	{
+	  // Successfully retrieved the long path
+	  break;
+	}
+    }
+  ret = !strcmp (lbasename (longPath), lbasename (xtensa_dynconfig_file));
+  free (longPath);
+  return ret;
+}
+
+#endif // HAVE_WINDOWS_H
+
 static char *get_xtensa_dynconfig_file (void)
 {
   const char *xtensa_dynconfig_env = getenv (CONFIG_ENV_NAME);
@@ -119,9 +176,16 @@ static char *get_xtensa_dynconfig_file (void)
   if (strcmp (lbasename (xtensa_dynconfig_env),
               lbasename (xtensa_dynconfig_file)))
     {
-      _bfd_error_handler (_("Both %s and \"-dynconfig=\" specified but pointed different files: \"%s\" \"%s\""),
-			      CONFIG_ENV_NAME, xtensa_dynconfig_env, xtensa_dynconfig_file);
-      abort ();
+#ifdef HAVE_WINDOWS_H
+      if (!is_8dot3_filename (xtensa_dynconfig_env))
+#endif
+	{
+	  _bfd_error_handler (_
+			      ("Both %s and \"-dynconfig=\" specified but pointed different files: \"%s\" \"%s\""),
+			      CONFIG_ENV_NAME, xtensa_dynconfig_env,
+			      xtensa_dynconfig_file);
+	  abort ();
+	}
     }
   /* XTENSA_GNU_CONFIG and mdynconfig option point to the same file */
   return xstrdup (xtensa_dynconfig_env);
@@ -134,44 +198,44 @@ const void *xtensa_load_config (const char *name ATTRIBUTE_UNUSED,
 				const void *no_name_def ATTRIBUTE_UNUSED)
 {
   static int init;
-  if (bfd_plugin_enabled ())
+#if BFD_SUPPORTS_PLUGINS
+  static void *handle;
+  void *p;
+
+  if (!init)
     {
-      static void *handle;
-      void *p;
+      char *path = get_xtensa_dynconfig_file();
 
-      if (!init)
+      init = 1;
+      if (!path)
+	return no_plugin_def;
+      handle = dlopen (path, RTLD_LAZY);
+      free (path);
+      if (!handle)
 	{
-	  const char *path = getenv (CONFIG_ENV_NAME);
-
-	  init = 1;
-	  if (!path)
-	    return no_plugin_def;
-	  handle = dlopen (path, RTLD_LAZY);
-	  if (!handle)
-	    {
-	      _bfd_error_handler (_("%s is defined but could not be loaded: %s"),
-				  CONFIG_ENV_NAME, dlerror ());
-	      abort ();
-	    }
-	}
-      else if (!handle)
-	{
-	  return no_plugin_def;
-	}
-
-      p = dlsym (handle, name);
-      if (!p)
-	{
-	  if (no_name_def)
-	    return no_name_def;
-
-	  _bfd_error_handler (_("%s is loaded but symbol \"%s\" is not found: %s"),
-			      CONFIG_ENV_NAME, name, dlerror ());
+	  _bfd_error_handler (_("%s is defined but could not be loaded (%s): %s"),
+			      CONFIG_ENV_NAME, path, dlerror ());
 	  abort ();
 	}
-      return p;
     }
-  else if (!init)
+  else if (!handle)
+    {
+      return no_plugin_def;
+    }
+
+  p = dlsym (handle, name);
+  if (!p)
+    {
+      if (no_name_def)
+	return no_name_def;
+
+      _bfd_error_handler (_("%s is loaded but symbol \"%s\" is not found: %s"),
+			  CONFIG_ENV_NAME, name, dlerror ());
+      abort ();
+    }
+  return p;
+#else
+  if (!init)
     {
       const char *path = strcmp(xtensa_dynconfig_file, "") ? xtensa_dynconfig_file : getenv (CONFIG_ENV_NAME);
 
@@ -184,6 +248,7 @@ const void *xtensa_load_config (const char *name ATTRIBUTE_UNUSED,
 	}
     }
   return no_plugin_def;
+#endif
 }
 
 XTENSA_CONFIG_INSTANCE_LIST;
